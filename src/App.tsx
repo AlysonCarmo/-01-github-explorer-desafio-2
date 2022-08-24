9fe8d167f436f4339b5fc303b3478b9b43247786
Silva Jose <jose.silva@ada.tec.br>
2022-08-23 23:51:59 -0300

    inicio

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
+import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
 import { api } from './services/api';
@@ -58,24 +58,10 @@ export function App() {
     setSelectedGenreId(id);
   }
 
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar {...genres}/>
 
       <div className="container">
         <header>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from './../components/Button';
+
+import './../styles/sidebar.scss';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+  
+}
+
+
+
+export function SideBar(props: GenreResponseProps[]) {
+
+  //const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  console.log(typeof props) 
+  console.log(props)
+   return (
+   <nav className="sidebar">
+   <span>Watch<p>Me</p></span>
+
+   <div className="buttons-container">
+    
+     
+   </div>
+
+ </nav>)
 }
